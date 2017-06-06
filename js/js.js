845484aa2ae4b1febcfaedc558d882a814719c25
AndresRiveroLedo <andresriveroformacion@gmail.com>
2017-06-06 18:55:11 +0200

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,34 +1,36 @@
-var numero = 1;
+/*variable que nos ayudara a leer el Json*/
+var num = 1;
 
 $(document).ready(function () {
     //cargar noticias
-
+    /*Cuando clickamos en el boton "mas noticias cargar los dos Json*/
     $('#masNoticias').click(function () {
-        if (numero <= 2) {
-            cargarJson();
+        if (num <= 2) {
+            getJson();
         } else {
             $('#masNoticias').hide();
-            alert("No hay mas noticias");
+            alert("No hay mas noticias");//en caso de no haya más documento json mostrara el mensaje
         }
     });
+    /*Cargara directamente los Json con cuando hagamos scroll en la web */
     $(window).scroll(function () {
         if ($(window).scrollTop() + $(window).height() > $(document).height() - 500) {
-            cargarJson();
+            getJson();
         }
     });
 });
-
-function cargarNoticias(json) {
+/*Funcion que introduce el json en las diferentes etiquetas señaladas del html*/
+function getNews(json) {
     $.each(json, function (i, noticias) {
         $('#noticia').append($('<div id="noticia1" class="container-fluid"> <section class="row"> <h2 class="col-sm-12 well well-sm"><a href="">'+noticias.titular+'</a></h2>   <div class="col-sm-12"><span class="glyphicon glyphicon-calendar">'+noticias.datetime+ '</span></div>    <div class="col-sm-4"> <img class="img-circle img-responsive center-block" src="' + noticias.img + '" alt="imageNoticia"></div>  <div class="col-sm-6"><p>'+noticias.desa+'</p></div>'));
     });
 }
-
-function cargarJson() {
-    if (numero <= 2) {
-        $.getJSON("json/" + numero + ".json", function (jsonObject) {
-            cargarNoticias(jsonObject);
+/*Funcion que carga el Json*/
+function getJson() {
+    if (num <= 2) {
+        $.getJSON("json/" + num + ".json", function (jsonObject) {
+            getNews(jsonObject);
         });
     }
-    numero++;
+    num++;
 }
